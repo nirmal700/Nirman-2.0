9567dceb641d2cd0aa1ead2fc7f1d4b3aa0519f6
Nirmal Kumar <75884340+nirmal700@users.noreply.github.com>
2022-09-28 14:48:54 +0530

    Temp

---
+++ Nirman20/app/src/main/java/com/sipc/silicontech/nirman20/Admins/NewTeamData.java
@@ -0,0 +1,201 @@
+package com.sipc.silicontech.nirman20.Admins;
+
+public class NewTeamData {
+    String mEventParticipating,mTeamName,mProblemStat,mApproach,mCollegeName,mTeamLead,mTeamLeadPhone,mMem1Name,mMem1Phone,mMem2Name,mMem2Phone,mMem3Name,mMem3Phone,mMem4Name,mMem4Phone,mRound1Eva,mRound2Eva,mRound3Eva,mCoParticipant,mFinalMark;
+    boolean mCheckedIn;
+
+    public NewTeamData() {
+    }
+
+    public NewTeamData(String mEventParticipating, String mTeamName, String mProblemStat, String mApproach, String mCollegeName, String mTeamLead, String mTeamLeadPhone, String mMem1Name, String mMem1Phone, String mMem2Name, String mMem2Phone, String mMem3Name, String mMem3Phone, String mMem4Name, String mMem4Phone, String mRound1Eva, String mRound2Eva, String mRound3Eva, String mCoParticipant, String mFinalMark, boolean mCheckedIn) {
+        this.mEventParticipating = mEventParticipating;
+        this.mTeamName = mTeamName;
+        this.mProblemStat = mProblemStat;
+        this.mApproach = mApproach;
+        this.mCollegeName = mCollegeName;
+        this.mTeamLead = mTeamLead;
+        this.mTeamLeadPhone = mTeamLeadPhone;
+        this.mMem1Name = mMem1Name;
+        this.mMem1Phone = mMem1Phone;
+        this.mMem2Name = mMem2Name;
+        this.mMem2Phone = mMem2Phone;
+        this.mMem3Name = mMem3Name;
+        this.mMem3Phone = mMem3Phone;
+        this.mMem4Name = mMem4Name;
+        this.mMem4Phone = mMem4Phone;
+        this.mRound1Eva = mRound1Eva;
+        this.mRound2Eva = mRound2Eva;
+        this.mRound3Eva = mRound3Eva;
+        this.mCoParticipant = mCoParticipant;
+        this.mFinalMark = mFinalMark;
+        this.mCheckedIn = mCheckedIn;
+    }
+
+    public String getmEventParticipating() {
+        return mEventParticipating;
+    }
+
+    public void setmEventParticipating(String mEventParticipating) {
+        this.mEventParticipating = mEventParticipating;
+    }
+
+    public String getmTeamName() {
+        return mTeamName;
+    }
+
+    public void setmTeamName(String mTeamName) {
+        this.mTeamName = mTeamName;
+    }
+
+    public String getmProblemStat() {
+        return mProblemStat;
+    }
+
+    public void setmProblemStat(String mProblemStat) {
+        this.mProblemStat = mProblemStat;
+    }
+
+    public String getmApproach() {
+        return mApproach;
+    }
+
+    public void setmApproach(String mApproach) {
+        this.mApproach = mApproach;
+    }
+
+    public String getmCollegeName() {
+        return mCollegeName;
+    }
+
+    public void setmCollegeName(String mCollegeName) {
+        this.mCollegeName = mCollegeName;
+    }
+
+    public String getmTeamLead() {
+        return mTeamLead;
+    }
+
+    public void setmTeamLead(String mTeamLead) {
+        this.mTeamLead = mTeamLead;
+    }
+
+    public String getmTeamLeadPhone() {
+        return mTeamLeadPhone;
+    }
+
+    public void setmTeamLeadPhone(String mTeamLeadPhone) {
+        this.mTeamLeadPhone = mTeamLeadPhone;
+    }
+
+    public String getmMem1Name() {
+        return mMem1Name;
+    }
+
+    public void setmMem1Name(String mMem1Name) {
+        this.mMem1Name = mMem1Name;
+    }
+
+    public String getmMem1Phone() {
+        return mMem1Phone;
+    }
+
+    public void setmMem1Phone(String mMem1Phone) {
+        this.mMem1Phone = mMem1Phone;
+    }
+
+    public String getmMem2Name() {
+        return mMem2Name;
+    }
+
+    public void setmMem2Name(String mMem2Name) {
+        this.mMem2Name = mMem2Name;
+    }
+
+    public String getmMem2Phone() {
+        return mMem2Phone;
+    }
+
+    public void setmMem2Phone(String mMem2Phone) {
+        this.mMem2Phone = mMem2Phone;
+    }
+
+    public String getmMem3Name() {
+        return mMem3Name;
+    }
+
+    public void setmMem3Name(String mMem3Name) {
+        this.mMem3Name = mMem3Name;
+    }
+
+    public String getmMem3Phone() {
+        return mMem3Phone;
+    }
+
+    public void setmMem3Phone(String mMem3Phone) {
+        this.mMem3Phone = mMem3Phone;
+    }
+
+    public String getmMem4Name() {
+        return mMem4Name;
+    }
+
+    public void setmMem4Name(String mMem4Name) {
+        this.mMem4Name = mMem4Name;
+    }
+
+    public String getmMem4Phone() {
+        return mMem4Phone;
+    }
+
+    public void setmMem4Phone(String mMem4Phone) {
+        this.mMem4Phone = mMem4Phone;
+    }
+
+    public String getmRound1Eva() {
+        return mRound1Eva;
+    }
+
+    public void setmRound1Eva(String mRound1Eva) {
+        this.mRound1Eva = mRound1Eva;
+    }
+
+    public String getmRound2Eva() {
+        return mRound2Eva;
+    }
+
+    public void setmRound2Eva(String mRound2Eva) {
+        this.mRound2Eva = mRound2Eva;
+    }
+
+    public String getmRound3Eva() {
+        return mRound3Eva;
+    }
+
+    public void setmRound3Eva(String mRound3Eva) {
+        this.mRound3Eva = mRound3Eva;
+    }
+
+    public String getmCoParticipant() {
+        return mCoParticipant;
+    }
+
+    public void setmCoParticipant(String mCoParticipant) {
+        this.mCoParticipant = mCoParticipant;
+    }
+
+    public String getmFinalMark() {
+        return mFinalMark;
+    }
+
+    public void setmFinalMark(String mFinalMark) {
+        this.mFinalMark = mFinalMark;
+    }
+
+    public boolean ismCheckedIn() {
+        return mCheckedIn;
+    }
+
+    public void setmCheckedIn(boolean mCheckedIn) {
+        this.mCheckedIn = mCheckedIn;
+    }
+}
