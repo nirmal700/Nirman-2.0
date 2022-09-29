8d9933b0cfa8739045f419bbfa3e2d88cb829dd8
Nirmal Kumar <75884340+nirmal700@users.noreply.github.com>
2022-09-29 14:51:25 +0530

    SignUp

--- Nirman20/app/src/main/java/com/sipc/silicontech/nirman20/Users/UsersSignUp.java
+++ Nirman20/app/src/main/java/com/sipc/silicontech/nirman20/Users/UsersSignUp.java
@@ -1,25 +1,48 @@
 package com.sipc.silicontech.nirman20.Users;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ActivityOptions;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.Pair;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.sipc.silicontech.nirman20.Admins.AddNewTeams;
 import com.sipc.silicontech.nirman20.Admins.AdminDashboard;
 import com.sipc.silicontech.nirman20.Admins.AdminSignin;
+import com.sipc.silicontech.nirman20.Admins.NewTeamData;
 import com.sipc.silicontech.nirman20.R;
+
+import java.util.ArrayList;
 
 public class UsersSignUp extends AppCompatActivity {
     ImageView bckBtn;
     Button next, login;
     TextView title;
+    AutoCompleteTextView mEventType,mTeam;
+    NewTeamData teamData;
+    ArrayList<String> arrayListPartcipantType;
+    ArrayAdapter<String> arrayAdapterPartcipantType;
+    ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +53,67 @@ public class UsersSignUp extends AppCompatActivity {
         next = findViewById(R.id.btn_next);
         login = findViewById(R.id.btn_login);
         title = findViewById(R.id.title);
+        mEventType = findViewById(R.id.autoCompleteEvent);
+        mTeam = findViewById(R.id.autoCompleteTeam);
+
+        progressDialog = new ProgressDialog(UsersSignUp.this);
+        progressDialog.show();
+        progressDialog.setCancelable(false);
+        progressDialog.setContentView(R.layout.progress_dialog);
+        progressDialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+        progressDialog.dismiss();
+
+        ArrayList<String> arrayListEventType;
+        ArrayAdapter<String> arrayAdapterEventType;
+        arrayListEventType = new ArrayList<>();
+        arrayListEventType.add("Robo Race");
+        arrayListEventType.add("Line Follower");
+        arrayListEventType.add("Ideate");
+        arrayListEventType.add("HackNation");
+        arrayAdapterEventType = new ArrayAdapter<>(getApplicationContext(), R.layout.text_menu, arrayListEventType);
+        mEventType.setAdapter(arrayAdapterEventType);
+
+        mEventType.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                mTeam.setText("Select Team Name");
+                progressDialog.show();
+                CollectionReference mCollectionReference = FirebaseFirestore.getInstance().collection(arrayAdapterEventType.getItem(i).toString());
+                mCollectionReference.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        progressDialog.cancel();
+                        if(task.isSuccessful())
+                        {
+                            arrayListPartcipantType = new ArrayList<>();
+                            arrayAdapterPartcipantType = new ArrayAdapter<>(getApplicationContext(), R.layout.text_menu, arrayListPartcipantType);
+                            for(QueryDocumentSnapshot document : task.getResult())
+                            {
+                                teamData = document.toObject(NewTeamData.class);
+                                Log.e("TAG", "onComplete: "+document.getId() +"=>"+document.getData() );
+                                arrayListPartcipantType.add(teamData.getmTeamName());
+                                arrayAdapterPartcipantType = new ArrayAdapter<>(getApplicationContext(), R.layout.text_menu, arrayListPartcipantType);
+                                mTeam.setAdapter(arrayAdapterPartcipantType);
+                            }
+                        }
+                        else
+                        {
+                            Log.e("TAG", "onComplete: Error Getting Documents"+task.getException() );
+                        }
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        progressDialog.cancel();
+                        Toast.makeText(UsersSignUp.this, "Error! "+e.toString(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+
+
+
+
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
