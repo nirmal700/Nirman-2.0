4a0a56724535b66f63e9a93a0c66130ac68353e8
Nirmal Kumar <75884340+nirmal700@users.noreply.github.com>
2022-10-10 18:18:22 +0530

    Update UserDashBoard.java

--- Nirman20/app/src/main/java/com/sipc/silicontech/nirman20/Users/UserDashBoard.java
+++ Nirman20/app/src/main/java/com/sipc/silicontech/nirman20/Users/UserDashBoard.java
@@ -1,16 +1,93 @@
 package com.sipc.silicontech.nirman20.Users;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.os.Build;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextClock;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.airbnb.lottie.LottieAnimationView;
+import com.google.android.material.card.MaterialCardView;
+import com.google.android.material.navigation.NavigationView;
 import com.sipc.silicontech.nirman20.R;
 
-public class UserDashBoard extends AppCompatActivity {
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
+public class UserDashBoard extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+    static final float END_SCALE = 0.7f;
+
+    String AES = "AES";
+    String keyPass = "Nirman@2023-SIPC";
+    MaterialCardView mGenQR;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    LinearLayout contentView;
+    TextView user_Name;
+    View nav_headerView;
+
+    TextView tv_date;
+    TextClock tv_time;
+
+
+    String phoneNo;
+    String view_date= new SimpleDateFormat("d MMM yyyy", Locale.getDefault()).format(new Date());
+
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.user_dashboard);
+        LottieAnimationView lottieAnimationView1 = findViewById(R.id.drawer_btn);
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
+    }
+    // Navigation Drawer Functions
+    private void navigationDrawer() {
+
+        navigationView.bringToFront();
+        navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setCheckedItem(R.id.nav_home);
+
+
+
+        animateNavigationDrawer();
+    }
+    private void animateNavigationDrawer() {
+
+        //Add any color or remove it to use the default one!
+        //drawerLayout.setScrimColor(getResources().getColor(R.color.red));
+        //To make it transparent use Color.Transparent in side setScrimColor();
+        //drawerLayout.setScrimColor(Color.TRANSPARENT);
+        drawerLayout.addDrawerListener(new DrawerLayout.SimpleDrawerListener() {
+            @Override
+            public void onDrawerSlide(View drawerView, float slideOffset) {
+
+                // Scale the View based on current slide offset
+                final float diffScaledOffset = slideOffset * (1 - END_SCALE);
+                final float offsetScale = 1 - diffScaledOffset;
+                contentView.setScaleX(offsetScale);
+                contentView.setScaleY(offsetScale);
+
+                // Translate the View, accounting for the scaled width
+                final float xOffset = drawerView.getWidth() * slideOffset;
+                final float xOffsetDiff = contentView.getWidth() * diffScaledOffset / 2;
+                final float xTranslation = xOffset - xOffsetDiff;
+                contentView.setTranslationX(xTranslation);
+            }
+        });
     }
 }
