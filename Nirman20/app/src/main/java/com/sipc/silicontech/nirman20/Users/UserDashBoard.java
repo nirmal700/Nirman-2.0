595a12512c0e786fa85a4a6468e87bb6ea050cb8
Nirmal Kumar <75884340+nirmal700@users.noreply.github.com>
2022-10-11 01:04:42 +0530

    QR Code Added

--- Nirman20/app/src/main/java/com/sipc/silicontech/nirman20/Users/UserDashBoard.java
+++ Nirman20/app/src/main/java/com/sipc/silicontech/nirman20/Users/UserDashBoard.java
@@ -1,21 +1,32 @@
 package com.sipc.silicontech.nirman20.Users;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextClock;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.airbnb.lottie.LottieAnimationView;
 import com.google.android.material.card.MaterialCardView;
 import com.google.android.material.navigation.NavigationView;
+import com.sipc.silicontech.nirman20.Admins.SessionManagerAdmin;
 import com.sipc.silicontech.nirman20.R;
 
 import java.text.SimpleDateFormat;
@@ -32,7 +43,7 @@ public class UserDashBoard extends AppCompatActivity implements NavigationView.O
     DrawerLayout drawerLayout;
     NavigationView navigationView;
     LinearLayout contentView;
-    TextView user_Name;
+    TextView user_Name,team_name,risk_level;
     View nav_headerView;
 
     TextView tv_date;
@@ -41,6 +52,9 @@ public class UserDashBoard extends AppCompatActivity implements NavigationView.O
 
     String phoneNo;
     String view_date= new SimpleDateFormat("d MMM yyyy", Locale.getDefault()).format(new Date());
+    SessionManagerParticipant managerParticipant;
+    SessionManagerAdmin managerAdmin;
+
 
     @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
     @Override
@@ -48,13 +62,50 @@ public class UserDashBoard extends AppCompatActivity implements NavigationView.O
         super.onCreate(savedInstanceState);
         setContentView(R.layout.user_dashboard);
         LottieAnimationView lottieAnimationView1 = findViewById(R.id.drawer_btn);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.navigation_view);
+        contentView = findViewById(R.id.linear_content);
+        team_name = findViewById(R.id.team_name);
+        user_Name = findViewById(R.id.participant_name);
+        risk_level = findViewById(R.id.mRiskLevel);
+        mGenQR = findViewById(R.id.btn_generateQR);
+
+        managerParticipant = new SessionManagerParticipant(getApplicationContext());
+        managerAdmin = new SessionManagerAdmin(getApplicationContext());
+        if (!isConnected(UserDashBoard.this)){
+            showCustomDialog();
+        }
+        user_Name.setText(managerParticipant.getParticipantName().toString());
+        team_name.setText(managerParticipant.getTeamName().toString());
+
+        Menu menuNav = navigationView.getMenu();
+
+        nav_headerView = navigationView.inflateHeaderView(R.layout.menu_header);
+        navigationDrawer();
+
+        lottieAnimationView1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                lottieAnimationView1.playAnimation();
+                lottieAnimationView1.loop(true);
+
+                if (drawerLayout.isDrawerVisible(GravityCompat.START))
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                else drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+
+        mGenQR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), UserQrCode.class));
+            }
+        });
+
 
     }
 
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        return false;
-    }
     // Navigation Drawer Functions
     private void navigationDrawer() {
 
@@ -65,6 +116,55 @@ public class UserDashBoard extends AppCompatActivity implements NavigationView.O
 
 
         animateNavigationDrawer();
+    }
+    @Override
+    public void onBackPressed() {
+
+        if (drawerLayout.isDrawerVisible(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+
+        } else
+            super.onBackPressed();
+    }
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        int id = item.getItemId();
+
+        switch (id){
+
+
+            case R.id.nav_home:
+                Toast.makeText(getApplicationContext(), "Home", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.nav_contactUs:
+                Toast.makeText(getApplicationContext(), "Contact Us", Toast.LENGTH_SHORT).show();
+                contactUs();
+                break;
+
+            case R.id.nav_share:
+                Toast.makeText(getApplicationContext(), "Share", Toast.LENGTH_SHORT).show();
+                share();
+                break;
+
+            case R.id.nav_about:
+                Toast.makeText(getApplicationContext(), "About", Toast.LENGTH_SHORT).show();
+                about();
+                break;
+
+            case R.id.logout:
+                logout();
+                break;
+
+            case R.id.exit:
+                Toast.makeText(this, "Thank you :)", Toast.LENGTH_SHORT).show();
+                finishAffinity();
+                break;
+        }
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+
+        return true;
     }
     private void animateNavigationDrawer() {
 
@@ -89,5 +189,112 @@ public class UserDashBoard extends AppCompatActivity implements NavigationView.O
                 contentView.setTranslationX(xTranslation);
             }
         });
+    }
+    private void share() {
+
+        try {
+
+            Intent i = new Intent(Intent.ACTION_SEND);
+            i.setType("text/plain");
+            i.putExtra(Intent.EXTRA_SUBJECT,"QR Registry");
+            i.putExtra(Intent.EXTRA_TEXT,"https://play.google.com/store/apps/details?id="+getApplicationContext().getPackageName());
+            startActivity(Intent.createChooser(i,"Share With"));
+
+        }catch (Exception e){
+            Toast.makeText(this, "Unable to share this app.", Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
+
+    private void contactUs() {
+        //startActivity(new Intent(getApplicationContext(), ContactUs.class));
+    }
+
+    private void about() {
+        //startActivity(new Intent(getApplicationContext(), AboutQrRegistry.class));
+    }
+    private void logout() {
+
+
+
+        //Initialize alert dialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        //Set Title
+        builder.setTitle("Log out");
+
+        //set Message
+        builder.setMessage("Are you sure to Log out ?");
+
+        //positive YES button
+        builder.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                managerParticipant.setParticipantLogin(false);
+                managerParticipant.setDetails("","","","","");
+
+                managerAdmin.setAdminLogin(false);
+                managerAdmin.setDetails("","","","","","");
+
+                //activity.finishAffinity();
+                dialog.dismiss();
+
+                //Finish Activity
+                startActivity(new Intent(getApplicationContext(), UsersSignUp.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK));
+                finish();
+            }
+        });
+
+        //Negative NO button
+        builder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //Dismiss Dialog
+                dialog.dismiss();
+            }
+        });
+
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+
+    //--------------- Internet Error Dialog Box -----------
+    private void showCustomDialog() {
+
+        android.app.AlertDialog.Builder builder = new android.app.AlertDialog.Builder(UserDashBoard.this);
+        builder.setMessage("Please connect to the internet")
+                //.setCancelable(false)
+                .setPositiveButton("Connect", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        startActivity(new Intent(Settings.ACTION_WIFI_SETTINGS));
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        startActivity(new Intent(getApplicationContext(),UserDashBoard.class));
+                        finish();
+                    }
+                });
+        android.app.AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+
+    }
+
+    //--------------- Check Internet Is Connected -----------
+    private boolean isConnected(UserDashBoard userDashBoard) {
+
+        ConnectivityManager connectivityManager = (ConnectivityManager) userDashBoard.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo wifiConn = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        NetworkInfo mobileConn = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+        NetworkInfo bluetoothConn = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_BLUETOOTH);
+
+        return (wifiConn != null && wifiConn.isConnected()) || (mobileConn != null && mobileConn.isConnected() || (bluetoothConn != null && bluetoothConn.isConnected())); // if true ,  else false
+
     }
 }
---
+++ Nirman20/app/src/main/java/com/sipc/silicontech/nirman20/Users/UserQrCode.java
@@ -0,0 +1,211 @@
+package com.sipc.silicontech.nirman20.Users;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.Settings;
+import android.util.Base64;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
+import com.sipc.silicontech.nirman20.R;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+
+public class UserQrCode extends AppCompatActivity {
+
+
+
+    ImageView btn_backToCd,qr_output;
+    ProgressDialog progressDialog;
+    TextView team_name;
+
+    //--------------- Encryption Variables -----------
+    String AES = "AES";
+    String keyPass = "qrregistry@user";
+    //------------------------------------------------
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_qr_code);
+
+        qr_output = findViewById(R.id.qr_output);
+        btn_backToCd  = findViewById(R.id.btn_backToCd);
+        team_name = findViewById(R.id.mTeam_name);
+        SessionManagerParticipant managerParticipant = new SessionManagerParticipant(getApplicationContext());
+        String phoneNo = managerParticipant.getPhone(); //Get Phone Number from session
+
+        //--------------- Internet Checking -----------
+        if (!isConnected(UserQrCode.this)){
+            showCustomDialog();
+        }
+        team_name.setText(managerParticipant.getTeamName().toString());
+
+        //--------------- Initialize ProgressDialog -----------
+        progressDialog = new ProgressDialog(UserQrCode.this);
+        progressDialog.show();
+        //progressDialog.setCancelable(false);
+        progressDialog.setContentView(R.layout.progress_dialog);
+        progressDialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+
+        //--------------- Data Access from User Database -----------
+        Query getCustomerDetails = FirebaseDatabase.getInstance().getReference("Participants").child(managerParticipant.getEventName()).child(phoneNo).child("Profile");
+        getCustomerDetails.addListenerForSingleValueEvent(new ValueEventListener() {
+            @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                //--------------- Access Data to Strings -----------
+                String appName = "Nirman 2.0";
+                String name = snapshot.child("mParticipantName").getValue(String.class);
+                String phoneNumber = snapshot.child("mPhoneNumber").getValue(String.class);
+                String teamname = snapshot.child("mTeamName").getValue(String.class);
+
+
+
+                //--------------- Encoding Data -----------
+                try {
+                    // assert phoneNumber != null;
+                    String encodedData = encrypt(managerParticipant.getEventName()+ ":" +name+ ":" +teamname+ ":" +phoneNumber);
+                    MultiFormatWriter writer = new MultiFormatWriter();
+
+                    //--------------- Create QR code -----------
+                    try {
+                        BitMatrix matrix = writer.encode( appName+":" + encodedData , BarcodeFormat.QR_CODE,350,350);
+
+                        BarcodeEncoder encoder =new BarcodeEncoder();
+                        Bitmap bitmap = encoder.createBitmap(matrix);
+                        qr_output.setImageBitmap(bitmap);
+
+                        progressDialog.dismiss();
+
+
+                    } catch (WriterException e) {
+                        e.printStackTrace();
+                    }
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        //--------------- Button for back to User Dashboard -----------
+        btn_backToCd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(UserQrCode.this,UserDashBoard.class));
+                finishAffinity();
+            }
+        });
+
+
+
+    }
+
+    //--------------- Internet Error Dialog Box -----------
+    private void showCustomDialog() {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(UserQrCode.this);
+        builder.setMessage("Please connect to the internet")
+                //   .setCancelable(false)
+                .setPositiveButton("Connect", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        startActivity(new Intent(Settings.ACTION_WIFI_SETTINGS));
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        startActivity(new Intent(getApplicationContext(),UserDashBoard.class));
+                        finish();
+                    }
+                });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+
+    }
+
+    //--------------- Check Internet Is Connected -----------
+    private boolean isConnected(UserQrCode userQrCode) {
+
+        ConnectivityManager connectivityManager = (ConnectivityManager) userQrCode.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo wifiConn = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        NetworkInfo mobileConn = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+        NetworkInfo bluetoothConn = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_BLUETOOTH);
+
+        return (wifiConn != null && wifiConn.isConnected()) || (mobileConn != null && mobileConn.isConnected() || (bluetoothConn != null && bluetoothConn.isConnected())); // if true ,  else false
+
+    }
+
+    //--------------- Encode Data -----------
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    private String encrypt(String forEncode) throws Exception{
+        SecretKeySpec key = generateKey(keyPass);
+        Cipher c = Cipher.getInstance(AES);
+        c.init(Cipher.ENCRYPT_MODE,key);
+        byte[] encVal = c.doFinal(forEncode.getBytes());
+        return Base64.encodeToString(encVal,Base64.DEFAULT);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    private SecretKeySpec generateKey(String keyPass) throws Exception {
+        final MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        byte[] bytes = keyPass.getBytes(StandardCharsets.UTF_8); //"UTF-8"
+        digest.update(bytes, 0, bytes.length);
+        byte[] key = digest.digest();
+        return new SecretKeySpec(key, "AES"); //SecretKeySpec secretKeySpec = new SecretKeySpec(key,"AES");
+
+    }
+
+
+
+
+
+}
