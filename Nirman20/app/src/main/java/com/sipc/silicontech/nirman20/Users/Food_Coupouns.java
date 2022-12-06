50fe074da24f4092b8b0dec0c99d6d7535f73308
Nirmal Kumar <75884340+nirmal700@users.noreply.github.com>
2022-12-07 02:33:58 +0530

    food coupon and request help

---
+++ Nirman20/app/src/main/java/com/sipc/silicontech/nirman20/Users/Food_Coupouns.java
@@ -0,0 +1,16 @@
+package com.sipc.silicontech.nirman20.Users;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.sipc.silicontech.nirman20.R;
+
+public class Food_Coupouns extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.food_coupouns);
+    }
+}
