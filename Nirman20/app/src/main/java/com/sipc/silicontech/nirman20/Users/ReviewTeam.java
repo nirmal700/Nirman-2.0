36a3b083b0c2032bbcaf14c76e21feb5c231846e
anisha2210 <90673812+anisha2210@users.noreply.github.com>
2022-12-21 18:40:42 +0530

    Review
    Review Co

---
+++ Nirman20/app/src/main/java/com/sipc/silicontech/nirman20/Users/ReviewTeam.java
@@ -0,0 +1,16 @@
+package com.sipc.silicontech.nirman20.Users;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.sipc.silicontech.nirman20.R;
+
+public class ReviewTeam extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_review_team);
+    }
+}
