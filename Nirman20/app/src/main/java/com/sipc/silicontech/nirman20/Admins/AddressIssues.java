c23d7dccb9f74334136c657448a6cdb7dd8a5c75
Nirmal Kumar <75884340+nirmal700@users.noreply.github.com>
2023-02-07 01:01:56 +0530

    Update AddressIssues.java

--- Nirman20/app/src/main/java/com/sipc/silicontech/nirman20/Admins/AddressIssues.java
+++ Nirman20/app/src/main/java/com/sipc/silicontech/nirman20/Admins/AddressIssues.java
@@ -1,20 +1,16 @@
 package com.sipc.silicontech.nirman20.Admins;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.app.ProgressDialog;
-import android.content.Intent;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.Toast;
-
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -22,27 +18,18 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.sipc.silicontech.nirman20.R;
 import com.sipc.silicontech.nirman20.Users.Help;
-import com.sipc.silicontech.nirman20.Users.RequestHelpAdapter;
-import com.sipc.silicontech.nirman20.Users.Request_Help;
 import com.sipc.silicontech.nirman20.Users.SessionManagerParticipant;
-import com.sipc.silicontech.nirman20.Users.UserDashBoard;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class AddressIssues extends AppCompatActivity {
     SessionManagerParticipant managerUser;
-
-
     ImageView btn_back;
     private RecyclerView recyclerView;
-
     private ProgressDialog progressDialog;
     private DatabaseReference mHelpDB;
-
-
     private List<Help> mHelp;
-    String helptype;
     private AddressIssuesAdapter adapter;
 
     @Override
@@ -59,12 +46,9 @@ public class AddressIssues extends AppCompatActivity {
         mHelpDB = FirebaseDatabase.getInstance().getReference("EventIssues");
 
 
-        btn_back.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(AddressIssues.this, AdminDashboard.class));
-                finishAffinity();
-            }
+        btn_back.setOnClickListener(v -> {
+            startActivity(new Intent(AddressIssues.this, AdminDashboard.class));
+            finishAffinity();
         });
 
         //Initialize ProgressDialog
@@ -73,6 +57,7 @@ public class AddressIssues extends AppCompatActivity {
         list(); // Load all data
 
     }
+
     //-----------------------Progress Dialog-------------------
     private void loadProgressDialog() {
 
@@ -83,18 +68,19 @@ public class AddressIssues extends AppCompatActivity {
         progressDialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
 
     }
+
     private void list() {
 
         mHelpDB.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 mHelp.clear();
-                for ( DataSnapshot snapshot : dataSnapshot.getChildren()){
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
                     for (DataSnapshot snapshot1 : snapshot.getChildren()) {
-                        for (DataSnapshot snapshot2 : snapshot1.getChildren()){
-                            for (DataSnapshot snapshot3 : snapshot2.getChildren()){
+                        for (DataSnapshot snapshot2 : snapshot1.getChildren()) {
+                            for (DataSnapshot snapshot3 : snapshot2.getChildren()) {
                                 Help help = snapshot3.getValue(Help.class);
-                                mHelp.add(0,help);
+                                mHelp.add(0, help);
                             }
                         }
                     }
@@ -104,14 +90,11 @@ public class AddressIssues extends AppCompatActivity {
                 recyclerView.smoothScrollToPosition(0);
                 progressDialog.dismiss();
             }
+
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
                 Toast.makeText(AddressIssues.this, error.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
-
-
-
-
     }
 }
