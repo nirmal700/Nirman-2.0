93da1c9f2c8779b444bf7cff83da34adae706bbc
Nirmal Kumar <75884340+nirmal700@users.noreply.github.com>
2023-02-07 01:19:11 +0530

    Update PublishResults.java

--- Nirman20/app/src/main/java/com/sipc/silicontech/nirman20/Admins/PublishResults.java
+++ Nirman20/app/src/main/java/com/sipc/silicontech/nirman20/Admins/PublishResults.java
@@ -1,20 +1,16 @@
 package com.sipc.silicontech.nirman20.Admins;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.util.Log;
 
-import com.google.android.gms.tasks.OnSuccessListener;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
 import com.sipc.silicontech.nirman20.R;
+
+import java.util.Objects;
 
 public class PublishResults extends AppCompatActivity {
 
@@ -24,34 +20,23 @@ public class PublishResults extends AppCompatActivity {
         setContentView(R.layout.publish_results);
 
         CollectionReference mCollectionReference = FirebaseFirestore.getInstance().collection("HackNation");
-        mCollectionReference.get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
-            @Override
-            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
-                for (DocumentSnapshot document1 : queryDocumentSnapshots.getDocuments()) {
-                    Log.d("45567Hack", document1.getId() + " => " + document1.getData());
-                    FirebaseFirestore.getInstance().collection("HackNation Evaluation").whereEqualTo("mTeamName",document1.get("mTeamName")).addSnapshotListener(new EventListener<QuerySnapshot>() {
-                        @Override
-                        public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
-                            double sum = 0;
-                            Log.e("45567", "onSuccess: "+value.getDocuments().toString() );
-                            for (DocumentSnapshot document : value.getDocuments()) {
-                                Log.d("45567InnerFOr", document.getId() + " => " + document.getData());
-                                sum =sum+(double) document.get("mAvg");
-                            }
-                            double avg = sum/value.size();
-                            Log.e("45567AVg", "onSuccess: "+ avg +"=>>"+document1.get("mTeamName"));
-                            FirebaseFirestore.getInstance().collection("HackNation").document(document1.get("mTeamName").toString()).update("mRound1Eva",avg)
-                                    .addOnSuccessListener(new OnSuccessListener<Void>() {
-                                        @Override
-                                        public void onSuccess(Void unused) {
-                                            Log.e("45567Update", "onSuccess: " );
-                                        }
-                                    });
-                        }
-                    });
-                }
-
+        mCollectionReference.get().addOnSuccessListener(queryDocumentSnapshots -> {
+            for (DocumentSnapshot document1 : queryDocumentSnapshots.getDocuments()) {
+                Log.d("45567Hack", document1.getId() + " => " + document1.getData());
+                FirebaseFirestore.getInstance().collection("HackNation Evaluation").whereEqualTo("mTeamName", document1.get("mTeamName")).addSnapshotListener((value, error) -> {
+                    double sum = 0;
+                    Log.e("45567", "onSuccess: " + Objects.requireNonNull(value).getDocuments());
+                    for (DocumentSnapshot document : value.getDocuments()) {
+                        Log.d("45567InnerFOr", document.getId() + " => " + document.getData());
+                        sum = sum + (double) document.get("mAvg");
+                    }
+                    double avg = sum / value.size();
+                    Log.e("45567AVg", "onSuccess: " + avg + "=>>" + document1.get("mTeamName"));
+                    FirebaseFirestore.getInstance().collection("HackNation").document(Objects.requireNonNull(document1.get("mTeamName")).toString()).update("mRound1Eva", avg)
+                            .addOnSuccessListener(unused -> Log.e("45567Update", "onSuccess: "));
+                });
             }
+
         });
     }
 }
