041a6a8b2a4b6b628a9241d7e95ac9ffec97e3b3
Nirmal Kumar <75884340+nirmal700@users.noreply.github.com>
2023-02-07 01:34:39 +0530

    Update SessionManagerEvaluator.java

--- Nirman20/app/src/main/java/com/sipc/silicontech/nirman20/Evaluators/SessionManagerEvaluator.java
+++ Nirman20/app/src/main/java/com/sipc/silicontech/nirman20/Evaluators/SessionManagerEvaluator.java
@@ -7,37 +7,45 @@ public class SessionManagerEvaluator {
     // Initialize Variables
     SharedPreferences sharedPreferences;
     SharedPreferences.Editor editor;
-    public SessionManagerEvaluator(Context context){
-        sharedPreferences = context.getSharedPreferences("EvaluatorAppKey",0);
+
+    public SessionManagerEvaluator(Context context) {
+        sharedPreferences = context.getSharedPreferences("EvaluatorAppKey", 0);
         editor = sharedPreferences.edit();
         editor.apply();
     }
-    // Set Login
-    public void setEvaluatorLogin(boolean login){
-        editor.putBoolean("KEY_EVALUATOR_LOGIN",login);
-        editor.commit();
-    }
-    public boolean getEvaluatorLogin(){
 
-        return sharedPreferences.getBoolean("KEY_EVALUATOR_LOGIN",false);
+    public boolean getEvaluatorLogin() {
+
+        return sharedPreferences.getBoolean("KEY_EVALUATOR_LOGIN", false);
     }
-    public void setEvaluatorDetails(String name,String event,String password,String phoneNo){
-        editor.putString("KEY_EVALUATOR_NAME",name);
-        editor.putString("KEY_EVENT",event);
-        editor.putString("KEY_EVA_PASSWORD",password);
-        editor.putString("KEY_EVALUATOR_PHONE",phoneNo);
+
+    // Set Login
+    public void setEvaluatorLogin(boolean login) {
+        editor.putBoolean("KEY_EVALUATOR_LOGIN", login);
         editor.commit();
     }
-    public String getEvaluatorName(){
-        return sharedPreferences.getString("KEY_EVALUATOR_NAME","");
+
+    public void setEvaluatorDetails(String name, String event, String password, String phoneNo) {
+        editor.putString("KEY_EVALUATOR_NAME", name);
+        editor.putString("KEY_EVENT", event);
+        editor.putString("KEY_EVA_PASSWORD", password);
+        editor.putString("KEY_EVALUATOR_PHONE", phoneNo);
+        editor.commit();
     }
-    public String getEventAssigned(){
-        return sharedPreferences.getString("KEY_EVENT","");
+
+    public String getEvaluatorName() {
+        return sharedPreferences.getString("KEY_EVALUATOR_NAME", "");
     }
-    public String getEvaPassword(){
-        return sharedPreferences.getString("KEY_EVA_PASSWORD","");
+
+    public String getEventAssigned() {
+        return sharedPreferences.getString("KEY_EVENT", "");
     }
-    public String getPhone(){
-        return sharedPreferences.getString("KEY_EVALUATOR_PHONE","");
+
+    public String getEvaPassword() {
+        return sharedPreferences.getString("KEY_EVA_PASSWORD", "");
+    }
+
+    public String getPhone() {
+        return sharedPreferences.getString("KEY_EVALUATOR_PHONE", "");
     }
 }
